6b2c15b0ce822b7b3ed89b92a2c0b0d3915d709d
rjbagget <robert.j.baggett@intel.com>
2018-04-09 13:52:11 -0700

    Added Routing, Materials and Image assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule,
+  ],
+  providers: []
+})
+
+export class MaterialModule {}
